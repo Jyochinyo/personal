9b0cdd65284a5cbbe2fa7f6df1b5df94a6b33e81
Xu Chunyang <chunyang.xu@clobotics.com>
2022-07-29 13:56:28 +0800

    添加国际化

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import i18n from './i18n'
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(i18n)
+  .mount('#app')
--- src/views/homePage/homePage.vue
+++ src/views/homePage/homePage.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="homePage">
-    HomePage111
+    HomePage111 {{ t('Apple') }}
   </div>
 </template>
 
 <script lang="ts" setup>
+
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
 </script>
 
 <style scoped lang="less">

6031d34a975e80f7c40ce2d8293883e9f9fb45df
jyochinyo <jyochinyo@hotmail.com>
2022-08-01 08:45:31 +0800

    menu

--- src/components/navMain/navMain.vue
+++ src/components/navMain/navMain.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="compContent">
-    <div class="navContent">
+    <div class="navContent" :class="[isMenuExpand ? 'expand' : 'collapse']">
       <div class="logoBg">
       </div>
       <div class="thumb" @click="toggleMenu">
@@ -8,21 +8,26 @@
         <div class="thumbTwo"></div>
         <div class="thumbThree"></div>
       </div>
+      <div class="menuList">
+        <a href="">HOME</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 /** 引入 import */
 
 /** 组件 component */
 
 /** 数据 data */
-// const myName = ref<string>('vue')
+const isMenuExpand = ref<boolean>(false);
 
 /** 函数 methods */
 const toggleMenu = () => {
   console.log('toggleMenu')
+  isMenuExpand.value = !isMenuExpand.value
 }
 
 </script>
@@ -37,19 +42,16 @@ const toggleMenu = () => {
   width: 125px;
   height: 100vh;
   padding: 30px;
-  .navContent{
-    background-color: red;
+  .navContent {
+    // background-color: red;
     width: 100%;
     height: 100%;
     position: relative;
     .logoBg {
       position: absolute;
-      left: 2px;
-      top: 76px;
-      width: 62px;
-      height: 62px;
-      border-radius: 50%;
       background-color: #f5c300;
+      transform: translateZ(0);
+      transition: all 0.5s;
     }
     .thumb {
       cursor: pointer;
@@ -57,15 +59,67 @@ const toggleMenu = () => {
       width: 33px;
       outline: none;
       position: absolute;
-      left: 16.5px;
-      top: 155px;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
+      left: 16.5px;
+      top: 155px;
       &>div {
         width: 100%;
         height: 3px;
         background-color: black;
+        transition: all 0.5s;
+        transform: translateX(0);
+      }
+    }
+    .menuList {
+      position: absolute;
+      top: 148px;
+      left: 74px;
+      padding: 7px 0;
+      &>a {
+        font-size: .85em;
+        font-weight: 600;
+        line-height: 1;
+        padding: 7px 0;
+        color: #fff;
+        text-decoration: none;
+      }
+    }
+  }
+  .collapse {
+    .logoBg {
+      left: 2px;
+      top: 76px;
+      width: 62px;
+      height: 62px;
+      border-radius: 50%;
+    }
+    .thumb {
+      left: 16.5px;
+      top: 155px;
+    }
+  }
+  .expand {
+    .logoBg {
+      top: 0;
+      left: 0;
+      width: 315px;
+      height: 100%;
+    }
+    .thumb {
+      left: 16.5px;
+      top: 155px;
+      .thumbOne {
+        transform: rotate(45deg) translate3d(5px,5px,0);
+        background-color: #fff;
+      }
+      .thumbTwo {
+        opacity: 0;
+      }
+      .thumbThree {
+        transform: rotate(-45deg) translate3d(5px,-5px,0);
+        background-color: #fff;
       }
     }
   }
